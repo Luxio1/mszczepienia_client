2bc9682e0eb8a49448955cf4d2d267322b10e150
pmorl <Patrykolux98*>
2021-11-14 17:39:52 +0100

    Add new color for buttons

--- lib/helpers/mycolors.dart
+++ lib/helpers/mycolors.dart
@@ -16,4 +16,20 @@ class MyColors {
       900: Color(0xFF0353A4),
     },
   );
+
+  static const MaterialColor lightBlue = MaterialColor(
+    0xFFB9D6F2,
+    <int, Color>{
+      50: Color(0xFFB9D6F2),
+      100: Color(0xFFB9D6F2),
+      200: Color(0xFFB9D6F2),
+      300: Color(0xFFB9D6F2),
+      400: Color(0xFFB9D6F2),
+      500: Color(0xFFB9D6F2),
+      600: Color(0xFFB9D6F2),
+      700: Color(0xFFB9D6F2),
+      800: Color(0xFFB9D6F2),
+      900: Color(0xFFB9D6F2),
+    },
+  );
 }
